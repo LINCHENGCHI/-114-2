18022a4598f306b69dbb77e49df67857eb1afea9
LINCHENGCHI <d11416112@gs.takming.edu.tw>
2026-06-29 00:15:15 +0800

    Add files via upload

---
+++ finaltest_5/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+public class Print
+{
+    public static string str = "";
+    public void printNum()
+    {
+        lock (this)
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                str += Thread.CurrentThread.Name + "[" + i + "]" + "\n";
+                Thread.Sleep(new Random().Next(5, 20));
+            }
+            str += "\n";
+        }
+    }
+}
---
+++ finaltest_5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Drawing;
+
+namespace _5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread[] tids = new Thread[3];
+            Print p1 = new Print();
+            for (int i = 0; i < 3; i++)
+            {
+                tids[i] = new Thread(new ThreadStart(p1.printNum));
+                tids[i].Name = "執行緒" + i + ":";
+            }
+            foreach (Thread t in tids)
+            {
+                t.Start();
+            }
+            Thread.Sleep(500);
+            Console.WriteLine(Print.str);
+        }
+    }
+}

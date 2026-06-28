81e910eded34a14941b2c830b69c6de97d85ee19
LINCHENGCHI <d11416112@gs.takming.edu.tw>
2026-06-29 00:16:06 +0800

    Add files via upload

---
+++ finaltest_4/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace finaltest_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDel hander = new MyDel(MyMath.Add);
+            hander += new MyDel(MyMath.Sub);
+            hander += new MyDel(MyMath.Mul);
+            hander += new MyDel(MyMath.Div);
+
+            MyMath.str = "委派1";
+            hander(20, 30);
+            Console.WriteLine(MyMath.str);
+
+            MyMath.str = "委派2";
+            hander -= new MyDel(MyMath.Add);
+            hander -= new MyDel(MyMath.Sub);
+            hander(15, 14);
+            Console.WriteLine(MyMath.str);
+        }
+    }
+}
